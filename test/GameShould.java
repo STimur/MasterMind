102f4a57f65456ff262a69bafc528db8a226f2ac
Timur <timur.salakhetdinov@gmail.com>
2017-12-31 11:37:42 +0300

    clean tests

--- test/GameShould.java
+++ test/GameShould.java
@@ -4,6 +4,16 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class GameShould {
+    private int[] intArray(int... ints) {
+        return ints;
+    }
+
+    private class GameMock extends Game {
+        public GameMock() {
+            code = new Code(intArray(1, 2, 3, 4));
+        }
+    }
+
     @Test
     public void
     not_be_finished_after_creation() {
@@ -15,15 +25,5 @@ public class GameShould {
     on_guess_return_number_of_wellplaced_and_misplaced_pegs() {
         assertThat(new GameMock().guess(intArray(0, 0, 0, 0)), is(intArray(0, 0)));
         assertThat(new GameMock().guess(intArray(1, 3, 2, 4)), is(intArray(2, 2)));
-    }
-
-    private int[] intArray(int... ints) {
-        return ints;
-    }
-
-    private class GameMock extends Game {
-        public GameMock() {
-            code = new Code(intArray(1, 2, 3, 4));
-        }
     }
 }
