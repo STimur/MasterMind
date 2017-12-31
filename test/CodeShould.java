216363efa6d7f16549e6b03abab6be4c8388c0db
Timur <timur.salakhetdinov@gmail.com>
2017-12-31 07:38:09 +0300

    clean tests

--- test/CodeShould.java
+++ test/CodeShould.java
@@ -10,9 +10,11 @@ public class CodeShould {
 
     @Test
     public void
-    calculate_well_placed_and_misplaced_pegs() {
+    calculate_wellplaced_pegs() {
         assertThat(new Code(intArray(0, 0, 0, 0)).guess(intArray(1, 1, 1, 1)), is(new int[]{0, 0}));
         assertThat(new Code(intArray(0, 0, 0, 0)).guess(intArray(0, 1, 1, 1)), is(new int[]{1, 0}));
         assertThat(new Code(intArray(0, 0, 0, 0)).guess(intArray(0, 0, 0, 0)), is(new int[]{4, 0}));
     }
+
+
 }
