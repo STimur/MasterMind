28a58f5530bdfee490f86e2d8985e3b392cd998f
Timur <timur.salakhetdinov@gmail.com>
2017-12-31 07:35:31 +0300

    clean tests

--- test/CodeShould.java
+++ test/CodeShould.java
@@ -4,11 +4,15 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CodeShould {
+    private int[] intArray(int... ints) {
+        return ints;
+    }
+
     @Test
     public void
     calculate_well_placed_and_misplaced_pegs() {
-        assertThat(new Code(new int[]{0, 0, 0, 0}).guess(new int[]{1, 1, 1, 1}), is(new int[]{0, 0}));
-        assertThat(new Code(new int[]{0, 0, 0, 0}).guess(new int[]{0, 1, 1, 1}), is(new int[]{1, 0}));
-        assertThat(new Code(new int[]{0, 0, 0, 0}).guess(new int[]{0, 0, 0, 0}), is(new int[]{4, 0}));
+        assertThat(new Code(intArray(0, 0, 0, 0)).guess(intArray(1, 1, 1, 1)), is(new int[]{0, 0}));
+        assertThat(new Code(intArray(0, 0, 0, 0)).guess(intArray(0, 1, 1, 1)), is(new int[]{1, 0}));
+        assertThat(new Code(intArray(0, 0, 0, 0)).guess(intArray(0, 0, 0, 0)), is(new int[]{4, 0}));
     }
 }
