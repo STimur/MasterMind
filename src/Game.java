a2297a00e659eef6f9c2034b64668f7e4bbf1838
Timur <timur.salakhetdinov@gmail.com>
2017-12-31 11:25:57 +0300

    add test for game

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,7 @@
 public class Game {
     public boolean isFinished;
+
+    public int[] guess(int[] ints) {
+        return new int[]{0, 0};
+    }
 }
--- test/GameShould.java
+++ test/GameShould.java
@@ -9,4 +9,14 @@ public class GameShould {
     not_be_finished_after_creation() {
         assertThat(new Game().isFinished, is(false));
     }
+
+    @Test
+    public void
+    on_guess_return_number_of_wellplaced_and_misplaced_pegs() {
+        assertThat(new Game().guess(intArray(0, 0, 0, 0)), is(intArray(0, 0)));
+    }
+
+    private int[] intArray(int... ints) {
+        return ints;
+    }
 }
