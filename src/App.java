5455852a5d59818d6f223db386d0646bc1597b0d
Timur <timur.salakhetdinov@gmail.com>
2018-01-02 18:25:39 +0300

    refactor main app

--- src/App.java
+++ src/App.java
@@ -3,29 +3,66 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class App {
+    private final Game game;
+    private final BufferedReader bufferedReader;
+    private String playerInput;
+
+    public App() {
+        game = new Game();
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
     public static void main(String[] args) {
-        Game game = new Game();
-        System.out.println("Enter you guess:");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (!game.isFinished) {
-            try {
-                String str = br.readLine();
-                if (str.length() > 4)
-                    throw new NumberFormatException();
-                int[] pegsGuess = new int[]{
-                        Integer.valueOf(Integer.valueOf(str.substring(0, 1))),
-                        Integer.valueOf(Integer.valueOf(str.substring(1, 2))),
-                        Integer.valueOf(Integer.valueOf(str.substring(2, 3))),
-                        Integer.valueOf(Integer.valueOf(str.substring(3, 4)))
-                };
-                int[] answer = game.guess(pegsGuess);
-                System.out.printf("wellplaced: %d misplaced: %d\n", answer[0], answer[1]);
-            } catch (NumberFormatException e) {
-                System.out.println("Wrong input! You should type 4 integers, e.g.: 1122");
-            } catch (IOException e) {
-                System.out.println("Something wrong with IO subsystem! Contact sysadmin, if the problem persists.");
-            }
-        }
+        new App().start();
+    }
+
+    private void start() {
+        while (!game.isFinished)
+            interactWithPlayer();
+        congratulatePlayer();
+    }
+
+    private void congratulatePlayer() {
         System.out.println("\nCongratulations! You broke the code.");
+    }
+
+    private void interactWithPlayer() {
+        try {
+            printGuessPromptMessage();
+            readPlayerInput();
+            validateInput();
+            printGuessResult();
+        } catch (NumberFormatException e) {
+            System.out.println("Wrong input! You should type 4 integers, e.g.: 1122\n");
+        } catch (IOException e) {
+            System.out.println("Something wrong with IO subsystem! Contact sysadmin, if the problem persists.\n");
+        }
+    }
+
+    private void printGuessPromptMessage() {
+        System.out.println("Enter you guess:");
+    }
+
+    private void printGuessResult() {
+        int[] answer = game.guess(parseInput(playerInput));
+        System.out.printf("wellplaced: %d misplaced: %d\n\n", answer[0], answer[1]);
+    }
+
+    private void readPlayerInput() throws IOException {
+        playerInput = bufferedReader.readLine();
+    }
+
+    private void validateInput() {
+        if (playerInput.length() != 4)
+            throw new NumberFormatException();
+    }
+
+    private int[] parseInput(String str) {
+        return new int[]{
+                Integer.valueOf(Integer.valueOf(str.substring(0, 1))),
+                Integer.valueOf(Integer.valueOf(str.substring(1, 2))),
+                Integer.valueOf(Integer.valueOf(str.substring(2, 3))),
+                Integer.valueOf(Integer.valueOf(str.substring(3, 4)))
+        };
     }
 }
