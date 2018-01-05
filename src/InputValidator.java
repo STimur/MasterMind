748f9436d6dd643f7360c1009a0af4d89d2b1619
Timur <timur.salakhetdinov@gmail.com>
2018-01-05 19:45:24 +0300

    add test for InputValidator

--- src/InputValidator.java
+++ src/InputValidator.java
@@ -2,6 +2,10 @@ public class InputValidator {
     public static void validate(String input) {
         if (input.length() != 4)
             throw new ValidationException();
+        for (char c : input.toCharArray())
+            if (c - '0' < 0 || c - '0' > 5)
+                throw new ValidationException();
+
 
     }
 
--- test/InputValidatorShould.java
+++ test/InputValidatorShould.java
@@ -6,4 +6,10 @@ public class InputValidatorShould {
     throw_exception_when_input_length_more_than_four() {
         InputValidator.validate("12345");
     }
+
+    @Test(expected = InputValidator.ValidationException.class)
+    public void
+    throw_exception_when_input_contain_letter() {
+        InputValidator.validate("a124");
+    }
 }
