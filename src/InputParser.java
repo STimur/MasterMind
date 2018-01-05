543c97fb27a7c6a4ab0f2ce6aa7ee513f2b5aad5
Timur <timur.salakhetdinov@gmail.com>
2018-01-05 19:54:11 +0300

    add InputParser class

---
+++ src/InputParser.java
@@ -0,0 +1,10 @@
+public class InputParser {
+    public static int[] parse(String input) {
+        return new int[]{
+                input.charAt(0) - '0',
+                input.charAt(1) - '0',
+                input.charAt(2) - '0',
+                input.charAt(3) - '0'
+        };
+    }
+}
