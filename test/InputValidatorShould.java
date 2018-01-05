c53fb377ed0f8fb1e3409f0b3d8fe8db10458654
Timur <timur.salakhetdinov@gmail.com>
2018-01-05 19:48:24 +0300

    add test for InputValidator

--- test/InputValidatorShould.java
+++ test/InputValidatorShould.java
@@ -3,6 +3,12 @@ import org.junit.Test;
 public class InputValidatorShould {
     @Test(expected = InputValidator.ValidationException.class)
     public void
+    throw_exception_when_input_length_less_than_four() {
+        InputValidator.validate("214");
+    }
+
+    @Test(expected = InputValidator.ValidationException.class)
+    public void
     throw_exception_when_input_length_more_than_four() {
         InputValidator.validate("12345");
     }
